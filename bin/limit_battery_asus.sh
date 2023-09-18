3d2be4cbaf44858a03685008295df954181bfa62
Maximilian Wolf <69987866+MaxWolf-01@users.noreply.github.com>
2023-09-18 14:32:43 +0200

    Add battery limiting script for asus

---
+++ bin/limit_battery_asus.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Credit: https://github.com/sreejithag/battery-charging-limiter-linux/tree/main
+
+max="$1"
+
+if [ "$#" -ge  1 ]; then
+	if echo $max | grep -E -q '^[0-9]+$'; then 
+		if [ "$max" -gt 100 ] || [ "$max" -le 0 ]; then
+			echo "Please eneter a valid max limit between [1-100]"
+		else
+			echo "Max battery capacity is limiting to $1%"
+			echo $max | sudo tee /sys/class/power_supply/BAT?/charge_control_end_threshold			
+		fi
+	else
+		echo "Please enter a numeric max value"
+	fi
+else
+	echo "Please enter the max limit and try again"
+fi
