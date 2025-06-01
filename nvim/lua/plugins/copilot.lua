54c61e2f9bbe7c0bdb60f31d5e220fc03c48eb01
Maximilian Wolf <69987866+MaxWolf-01@users.noreply.github.com>
2025-06-01 18:53:38 +0200

    add partial completion keymaps to copilot

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,22 +1,38 @@
 return {
   "github/copilot.vim",
   config = function()
+
     -- we'll handle all mappings ourselves
     vim.g.copilot_no_tab_map    = true
     vim.g.copilot_assume_mapped = true
+
     -- request a suggestion
-    vim.keymap.set("i", "<A-q>",
+    vim.keymap.set("i", "<A-r>",
       "copilot#Suggest()",
       { expr = true, silent = true, replace_keycodes = false }
     )
-    -- accept current suggestion
+
+    -- accept next word 
     vim.keymap.set("i", "<A-w>",
+      'copilot#AcceptWord()',
+      { expr = true, silent = true, replace_keycodes = false }
+    )
+
+    -- accept next line
+    vim.keymap.set("i", "<A-q>",
+      'copilot#AcceptLine()',
+      { expr = true, silent = true, replace_keycodes = false }
+    )
+
+    -- accept current suggestion
+    vim.keymap.set("i", "<A-s>",
       'copilot#Accept("")',
       { expr = true, silent = true, replace_keycodes = false }
     )
     vim.keymap.set("i", "<A-e>", "copilot#Dismiss()", { expr = true, silent = true })
     vim.keymap.set("i", "<A-d>", "copilot#Next()", { expr = true, silent = true })
     vim.keymap.set("i", "<A-a>", "copilot#Previous()", { expr = true, silent = true })
+
     -- toggle Copilot on/off (uses buffer-scoped flag set by the plugin)
     vim.keymap.set("n", "<leader>cc", function()
       if vim.b.copilot_suggestion_enabled then
@@ -25,5 +41,6 @@ return {
         vim.cmd("Copilot enable")
       end
     end, { silent = true, desc = "Toggle Copilot" })
+
   end,
 }
