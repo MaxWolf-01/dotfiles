d5d86734303617c5d06089e3c5910bcec52345f8
Maximilian Wolf <69987866+MaxWolf-01@users.noreply.github.com>
2025-12-03 00:46:52 +0100

    vesktop

--- nix/home/common.nix
+++ nix/home/common.nix
@@ -1,10 +1,17 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
+let
+  dotfiles = "${config.home.homeDirectory}/.dotfiles";
+in
 {
   home.username = "max";
   home.homeDirectory = "/home/max";
   home.stateVersion = "24.05";
 
   programs.home-manager.enable = true;
+
+  xdg.enable = true;
+
+  home.file.".config/vesktop/themes/custom.theme.css".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/desktop/discord/themes/custom.theme.css";
 
   home.packages = with pkgs; [
     btop
@@ -17,6 +24,7 @@
     ripgrep
     tmux
     tree
+    vesktop
     zoxide
   ];
 }
